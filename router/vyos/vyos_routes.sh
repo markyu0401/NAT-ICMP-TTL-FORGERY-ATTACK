c90ae7f91cccd8d8081cc8152edf6834c29230ba
markyu0401 <60618569+markyu0401@users.noreply.github.com>
2023-12-07 18:59:58 -0500

    Update vyos_routes.sh

--- router/vyos/vyos_routes.sh
+++ router/vyos/vyos_routes.sh
@@ -1,3 +1,3 @@
 ip route del default
-ip route add default via 172.16.100.0 dev eth1
+ip route add default via 172.16.100.1 dev eth0
 iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
